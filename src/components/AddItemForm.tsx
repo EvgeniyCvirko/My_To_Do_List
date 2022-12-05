ff594fc93eba8a6769ac38ee88bf1522d1fff95b
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-12-05 23:50:42 +0300

    feat: add AddItem

---
+++ src/components/AddItemForm.tsx
@@ -0,0 +1,25 @@
+import {ChangeEvent, KeyboardEvent, useState} from 'react';
+
+type AddItemFormType = {
+  addItem: (title: string) => void
+}
+export const AddItemForm = (props:AddItemFormType ) => {
+  const [value, setValue] = useState<string>('')
+  console.log(value)
+  const addFormHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value)
+  }
+  const onKeyHandler = (e:KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === 'Enter') {
+      props.addItem(value)
+      setValue('')
+    }
+  }
+
+  return <div>
+    <input autoFocus
+           value={value}
+           onChange={addFormHandler}
+           onKeyPress={onKeyHandler}/>
+  </div>
+}
