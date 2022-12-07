ce4a05db3ef437766360dbcb0130656c4399c6fb
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-12-07 23:37:44 +0300

    fix: header style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,14 @@ import { Todolists } from '../feature/Todolists/Todolists';
 import './App.css';
 
 export const App = () => {
+
+  const logoutHandler = () => {
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <div className='Todolist'>TodoList</div>
+        <div className='Logout' onClick={logoutHandler}>Logout</div>
       </header>
       <Todolists />
     </div>
