b77b722a625e62212ef260329196a3109d5df2da
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-12-15 23:17:12 +0300

    ref: new render Buttons

--- src/feature/Todolists/Todolists.tsx
+++ src/feature/Todolists/Todolists.tsx
@@ -10,36 +10,36 @@ import {AddItemForm} from '../../components/AddItemForm';
 export const Todolists = () => {
   const dispatch = useAppDispatch()
   const todolists = useAppSelector(state => state.todolists)
-  const isLogin =  useAppSelector(state => state.login.isLogin)
- const addTodolist = useCallback((title: string) => {
+  const isLogin = useAppSelector(state => state.login.isLogin)
+  const addTodolist = useCallback((title: string) => {
     dispatch(createTodolist(title))
- }, [])
+  }, [])
 
   const todolistRender = todolists.map((td) => {
-      return <div key={td.id} >
-        <div >
+      return <div key={td.id}>
+        <div>
           <Todolist todolist={td}/>
         </div>
       </div>
     }
   )
   useEffect(() => {
-    if(!isLogin){
+    if (!isLogin) {
       return
     }
     dispatch(getTodolists())
   }, [])
 
-  if(!isLogin){
-    return <Navigate to={'/login'} />
+  if (!isLogin) {
+    return <Navigate to={'/login'}/>
   }
 
-  return ( <div className={s.todolists}>
+  return (<div className={s.todolists}>
       <div className={s.addForm}><AddItemForm addItem={addTodolist}/></div>
       <div className={s.todolistRender}>
         {todolistRender}
       </div>
-  </div>
+    </div>
 
   )
 }
