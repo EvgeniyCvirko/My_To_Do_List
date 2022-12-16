864c9cb72e264c8314ed7dd59eb9a7a57192c84d
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-12-16 23:08:09 +0300

    ref: ctrl + alt + L

--- src/feature/Todolists/Todolist/Todolist.tsx
+++ src/feature/Todolists/Todolist/Todolist.tsx
@@ -13,6 +13,7 @@ type TodolistPropsType = {
 export const Todolist = (props: TodolistPropsType) => {
   const dispatch = useAppDispatch()
   const changeTitle = (title: string) => {
+
     dispatch(changeTodolistTitle({todolistId: props.todolist.id, title}))
   }
 
