c27de6a52d0593be52915799c89939ef99350f93
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-12-22 22:06:10 +0300

    fix: error handing for LoginReducer.ts

--- src/feature/Auth/LoginReducer.ts
+++ src/feature/Auth/LoginReducer.ts
@@ -1,33 +1,41 @@
 import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {LoginApi} from '../../api/LoginApi';
 import {LoginDataType} from '../../types/CommonTypes';
+import {ThunkError} from '../../api/Types';
+import {asyncServerAppError, asyncServerNetworkError} from '../../utils/error--utils';
 
 
 
 //thunk
-export const setLogin = createAsyncThunk(
-  'login/setLogin', async (param:{LoginData: LoginDataType}) => {
+export const setLogin = createAsyncThunk<{isLogin: boolean},{LoginData: LoginDataType}, ThunkError>(
+  'login/setLogin', async (param, thunkApi) => {
 
     try {
       const res = await LoginApi.setLogin(param.LoginData)
       if (res.data.resultCode === 0) {
         return {isLogin: true}
+      } else {
+        return asyncServerAppError(thunkApi, res.data)
       }
-
-    } catch {}
+    } catch (error: any) {
+      return asyncServerNetworkError(thunkApi, error)
+    }
   }
 )
 
-export const setLogout = createAsyncThunk(
-  'login/setLogout', async () => {
+export const setLogout = createAsyncThunk<{isLogin: boolean},undefined, ThunkError>(
+  'login/setLogout', async (param,thunkApi) => {
 
     try {
       const res = await LoginApi.deletLogin()
       if (res.data.resultCode === 0) {
         return {isLogin: false}
+      }else {
+        return asyncServerAppError(thunkApi, res.data)
       }
-
-    } catch {}
+    } catch (error: any) {
+      return asyncServerNetworkError(thunkApi, error)
+    }
   }
 )
 
@@ -44,14 +52,10 @@ export const slice = createSlice({
    },
   extraReducers: (builder) => {
     builder.addCase(setLogin.fulfilled, (state, action) => {
-      if (action.payload) {
         state.isLogin = action.payload.isLogin
-      }
     });
     builder.addCase(setLogout.fulfilled, (state, action) => {
-      if (action.payload) {
         state.isLogin = action.payload.isLogin
-      }
     });
   }
 })
